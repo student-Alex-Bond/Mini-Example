d810b84866a4516d9d55f48a294d341c545b87ce
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-02-05 09:02:29 +0600

    custom component select mine

--- src/Components/Select/Select.stories.tsx
+++ src/Components/Select/Select.stories.tsx
@@ -1,12 +1,10 @@
 import React, {useState} from "react";
 import {ComponentStory, ComponentMeta} from '@storybook/react';
-
 import Select from "./Select";
 
 
 export default {
     title: 'Components/Select Stories',
-
     component: Select,
 } as ComponentMeta<typeof Select>;
 
@@ -16,7 +14,7 @@ export const CustomSelect = Template.bind({})
 
 CustomSelect.args = {
     title: 'Users',
-
+    currentValue: 0,
     items: [{title: 'Dimych', value: 1},
         {title: 'Valera', value: 2},
         {title: 'Viktor', value: 3},
@@ -24,17 +22,37 @@ CustomSelect.args = {
 }
 
 
-export const GreatSelect: ComponentStory<typeof Select> = () => {
-    const [value, setValue] = useState<boolean>(false)
+export const GreatSelect: ComponentStory<typeof Select> = (args) => {
+
+    const [collapsed, setCollapsed] = React.useState<boolean>(true)
+
+    const [title, setTitle] = useState<string>('Users')
 
 
-    return <Select title={'Users'}
-                      currentValue={"2"}
-                      items={[{title: 'Dimych', value: 1},
-                          {title: 'Valera', value: 2},
-                          {title: 'Viktor', value: 3},
-                          {title: 'Artem', value: 4}]}
-                      />
+    const openClosedMenu = (flag: boolean) => {
+        setCollapsed(flag)
+    }
+
+    const onChangeCallback = (userID: string) => {
+        let newTitle = args.items.find((item) => item.value === userID)
+        if (newTitle) {
+            setTitle(newTitle.title)
+            setCollapsed(true)
+        }
+    }
+
+
+    return <Select title={title}
+                   onChange={onChangeCallback}
+                   openClosedMenu={openClosedMenu}
+                   collapsed={collapsed}
+                   items={args.items}/>
 }
-
+GreatSelect.args = {
+    items: [
+        {title: 'Dimych', value: '1'},
+        {title: 'Valera', value: '2'},
+        {title: 'Viktor', value: '3'},
+        {title: 'Artem', value: '4'}]
+}
 
--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,23 +1,58 @@
 import React from 'react';
+import styles from './Select.module.css'
+import arrow from './arrow.png'
 
-type ItemType = {
+export type ItemType = {
     title: string
     value: any
 }
 
 
 type SelectPropsType = {
+
     title: string
-    currentValue: any
+    currentValue?: any
+    collapsed: boolean
+    onClick: () => string
     onChange?: (value: any) => void
-    items: ItemType[]
+    items: ItemType[];
+    openClosedMenu:(flag: boolean) => void
 }
 
 const Select = (props: SelectPropsType) => {
+
+    let arrowStyle = styles.arrow
+    if (!props.collapsed) {
+        arrowStyle = styles.arrow_bottom
+    }
+
+    const userIdCallback = (event: any) => {
+        let userId = event.target.id
+        console.log(event)
+        if(userId){
+            //@ts-ignore
+            props.onChange(userId)
+        }
+    }
+
+    const openClosed =() => {
+        props.openClosedMenu(!props.collapsed)
+    }
     return (
         <div>
-            <div>{props.title}</div>
-            {props.items.map((item)=> <div>{item.title}</div>)}
+            <div onClick={openClosed}
+                 className={styles.title}>
+               { props.title }
+                <img className={arrowStyle} src={arrow} alt={'arrow'}/>
+
+            </div>
+            <div> {!props.collapsed &&
+                <div className={styles.item}>
+                    {props.items.map((item) =>
+                        <div className={styles.item} onClick={userIdCallback} id={item.value} key={item.value}>{item.title}</div>)}
+                </div>}
+            </div>
+
         </div>
     );
 };
